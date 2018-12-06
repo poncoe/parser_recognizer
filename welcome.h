902e51d607e3a6c1677012758fdbb258792a83dd
poncoe <poncoe4mac@gmail.com>
2018-12-06 23:45:04 +0700

    fix mixstake

--- welcome.h
+++ welcome.h
@@ -6,7 +6,7 @@ using namespace std;
 // Deklarasiin File yang ada di Welcome.CPP biar bisa dipanggil
 
 int welcomeIndo(); // deklarasiin file Welcome
-int valid(); // deklarasiin file Keluar Aplikasi
-int tidakValid(); // Deklarasiin ketika Menu Salah
+int valid(); // deklarasiin file valid
+int tidakValid(); // Deklarasiin tidak valid
 
 #endif // WELCOME_H_INCLUDED
